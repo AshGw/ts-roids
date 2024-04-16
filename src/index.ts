3ea0bd993231d8543fbaa42f6a0aed2853167654
AshGw <AshrefGw@proton.me>
2024-04-17 00:15:04 +0100

    feat: add `Falsy` type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 export type Nullable = null | undefined;
 export type Numeric = number | bigint;
 export type Primitive = Nullable | Numeric | string | boolean | symbol;
+export type Falsy = false | '' | 0 | Nullable;
 
