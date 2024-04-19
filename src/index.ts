7414c31769cc62da8556f674bd974b89c1fdeb84
AshGw <AshrefGw@proton.me>
2024-04-19 20:31:32 +0100

    chore: cleanup docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /**
- * `Nullable` export type allows representing a value that can be either null or undefined.
+ * `Nullable`  type allows representing a value that can be either null or undefined.
  * @export type {Nullable}
  */
 export type Nullable = null | undefined;
 
 /**
- * Represents a export type that can hold any numeric value: number or a bigint.
+ * Represents a  type that can hold any numeric value: number or a bigint.
  * @export type {Numeric}
  */
 export type Numeric = number | bigint;
@@ -17,102 +17,100 @@ export type Numeric = number | bigint;
  * - `string`: Represents textual data.
  * - `boolean`: Represents a logical value (true or false).
  * - `symbol`: Represents a unique and immutable value.
- * @export type {Primitive}
  */
 export type Primitive = string | boolean | symbol | Nullable | Numeric;
 
 /**
- * Represents a export type that includes falsy values in JavaScript.
+ * Represents a type that includes falsy values in JavaScript.
  * Falsy values are those that coerce to false when used in a boolean context.
  * This includes `false`, an empty string (`''`), numeric zero (`0`), `null`,
  * and `undefined`.
- * @export type {Falsy}
  */
 export type Falsy = false | '' | 0 | Nullable;
 
 /**
- * Checks if a given export type `T` is `Falsy`.
+ * Checks if a given type `T` is `Falsy`.
  * @returns `true` if `T` is a subtype of `Falsy`, otherwise `false`.
  * @example
- * export type Falsy = IsFalsy<''>; // TestFalsy is tru`
- * export type Truthy = IsFalsy<10>; // TestTruthy is false
+ * type Falsy = IsFalsy<''>; // TestFalsy is tru`
+ * type Truthy = IsFalsy<10>; // TestTruthy is false
  */
 export type IsFalsy<T> = T extends Falsy ? true : false;
 
 /**
- * Checks if a given export type `T` is a truthy value.
+ * Checks if a given  type `T` is a truthy value.
  * A truthy value is any value that is not a falsy value.
  * @returns `true` if `T` is not a subtype of `Falsy`, otherwise `false`.
  * @example
- * export type TruthyString = IsTruthy<string>; // => true
- * export type TruthyNumber = IsTruthy<10>; // => true
- * export type FalsyNull = IsTruthy<null>; // => false
- * export type FalsyEmptyString = IsTruthy<''>; => false
+ *  type TruthyString = IsTruthy<string>; // => true
+ *  type TruthyNumber = IsTruthy<10>; // => true
+ *  type FalsyNull = IsTruthy<null>; // => false
+ *  type FalsyEmptyString = IsTruthy<''>; => false
  */
 export type IsTruthy<T> = T extends Exclude<T, Falsy> ? true : false;
 
 /**
- * Checks if a given export type `T` is `never`.
- * The `never` export type represents a value that never occurs,
+ * Checks if a given  type `T` is `never`.
+ * The `never`  type represents a value that never occurs,
  * for example a function that always errors out.
  * @returns `true` if `T` is `never`, otherwise `false`.
  * @example
- * export type Never = IsNever<never>; => true
- * export type NotNever = IsNever<string>; => false
+ *  type Never = IsNever<never>; => true
+ *  type NotNever = IsNever<string>; => false
  */
 export type IsNever<T> = T extends never ? true : false;
 
 /**
- * Checks if a given export type `T` is `unknown`.
- * The `unknown` export type is the export type-safe counterpart of `any`.
- * Values of export type `unknown` can hold any value, similar to `any`, but with stricter export type safety.
- * Unlike `any`, you cannot perform operations directly on values of export type `unknown`
- * without export type assertion or export type narrowing.
+ * Checks if a given  type `T` is `unknown`.
+ * The `unknown`  type is the export type-safe counterpart of `any`.
+ * Values of  type `unknown` can hold any value, similar to `any`, but with stricter  type safety.
+ * Unlike `any`, you cannot perform operations directly on values of  type `unknown`
+ * without  type assertion or  type narrowing.
  * @returns `true` if `T` is `unknown`, otherwise `false`.
  * @example
- * export type UnknownValue = unknown;
- * export type IsUnknownValue = IsUnknown<UnknownValue>; =>  true
+ *  type UnknownValue = unknown;
+ *  type IsUnknownValue = IsUnknown<UnknownValue>; =>  true
  *
- * export type KnownValue = string;
- * export type IsNotUnknownValue = IsUnknown<KnownValue>; => true
+ *  type KnownValue = string;
+ *  type IsNotUnknownValue = IsUnknown<KnownValue>; => true
  */
 export type IsUnknown<T> = T extends unknown ? true : false;
 
 /**
- * Represents the keys of a given export type `T`.
- * This export type alias `Keys<T>` is equivalent to `keyof T`,
- * which retrieves the union export type of keys (property names) of export type `T`.
- * @returns Union export type of keys (property names) of export type `T`.
+ * Represents the keys of a given  type `T`.
+ * This  type alias `Keys<T>` is equivalent to `keyof T`,
+ * which retrieves the union  type of keys (property names) of  type `T`.
+ * @returns Union  type of keys (property names) of  type `T`.
  * @example
- * export type Person = {
+ *  type Person = {
  *   name: string;
  *   age: number;
  *   email: string;
  * };
  *
- * export type PersonKeys = Keys<Person>; => "name" | "age" | "email"
+ *  type PersonKeys = Keys<Person>; => "name" | "age" | "email"
  */
 export type Keys<T> = keyof T;
 
 /**
- * Represents the union export type of values of properties in a given export type `T`.
- * This export type alias `Vals<T>` retrieves the union export type of values corresponding
- * to the keys (property names) of export type `T`.
+ * Represents the union  type of values of properties in a given  type `T`.
+ * This  type alias `Vals<T>` retrieves the union  type of values corresponding
+ * to the keys (property names) of  type `T`.
  * @example
- * export type Person = {
+ *  type Person = {
  *   name: string;
  *   age: number;
  *   email: string;
  * };
  *
- * export type PersonValues = Vals<Person>; => string | number
+ *  type PersonValues = Vals<Person>; => string | number
  */
 export type Vals<T> = T[Keys<T>];
 
 /**
- * Represents a export type that can be either a single value of export type `T` or an array of values of export type `T`.
+ * Represents a  type that can be either a single value of  type `T` or an array of values of  type `T`.
  * @example
- * export type SingleOrArray<T> = OneOrMany<T>;
+ *  type SingleOrArray<T> = OneOrMany<T>;
  *
  * const value1: OneOrMany<number> = 10; // Valid
  * const value2: OneOrMany<number> = [20, 30]; // Also valid
@@ -120,54 +118,54 @@ export type Vals<T> = T[Keys<T>];
 export type EitherOneOrMany<T> = T | T[];
 
 /**
- * Represents a export type that can be used to construct a new instance.
- * This export type is typically used to describe constructor functions or classes
+ * Represents a  type that can be used to construct a new instance.
+ * This type is typically used to describe constructor functions or classes
  * that can be invoked using the `new` keyword.
  */
 export type Newable = { new (...args: any[]): any };
 
 /**
- * Checks if a given export type `T` is a export type that can be used to construct a new instance (i.e., a `Newable` export type).
- * The `Newable` export type typically represents constructor functions or classes that can be invoked using the `new` keyword.
+ * Checks if a given type `T` is a type that can be used to construct a new instance (i.e., a `Newable`  type).
+ * The `Newable` type typically represents constructor functions or classes that can be invoked using the `new` keyword.
  * @returns `true` if `T` is a `Newable`, otherwise `false`.
  */
 export type IsNewable<T> = T extends Newable ? true : false;
 
 /**
- * Represents a export type that describes any function accepting any arguments
+ * Represents a  type that describes any function accepting any arguments
  *  and returning any value.
  */
 export type AnyFunction = (...args: any[]) => any;
 
 /**
- * Represents a export type that describes any function accepting and retruning `unknown`s
+ * Represents a  type that describes any function accepting and retruning `unknown`s
  */
 export type UnknownFunction = (...args: unknown[]) => unknown;
 
 /**
- * Conditional export type that checks if export type `T` extends export type `P`.
- * If `T` extends `P`, the export type resolves to `Do`; otherwise, it resolves to `Else`.
+ * Conditional  type that checks if  type `T` extends  type `P`.
+ * If `T` extends `P`, the  type resolves to `Do`; otherwise, it resolves to `Else`.
  * @example
- * export type Result1 = IfExtends<string, string | number, true, false>; // is true
- * export type Result2 = IfExtends<number, string | number, true, false>; // is true
- * export type Result3 = IfExtends<boolean, string | number, true, false>; // is false
+ *  type Result1 = IfExtends<string, string | number, true, false>; // is true
+ *  type Result2 = IfExtends<number, string | number, true, false>; // is true
+ *  type Result3 = IfExtends<boolean, string | number, true, false>; // is false
  *
- * export type IsString<T> = IfExtends<T, string, true, false>;
- * export type IsNumber<T> = IfExtends<T, number, true, false>;
+ *  type IsString<T> = IfExtends<T, string, true, false>;
+ *  type IsNumber<T> = IfExtends<T, number, true, false>;
  *
- * export type TestString = IsString<string>; // is true
- * export type TestNumber = IsNumber<number>; // is true
- * export type TestBoolean = IsNumber<boolean>; // is false
+ *  type TestString = IsString<string>; // is true
+ *  type TestNumber = IsNumber<number>; // is true
+ *  type TestBoolean = IsNumber<boolean>; // is false
  */
 export type IfExtends<T, P, Do, Else> = T extends P ? Do : Else;
 
 /**
- * Conditional export type that checks if two types `X` and `Y` are exactly equal.
- * If `X` is equal to `Y`, the export type resolves to `true`; otherwise, it resolves to `false`.
+ * Conditional  type that checks if two types `X` and `Y` are exactly equal.
+ * If `X` is equal to `Y`, the  type resolves to `true`; otherwise, it resolves to `false`.
  * @example
- * export type Result1 = Equals<string, string>; // is true
- * export type Result2 = Equals<number, string>; // is false
- * export type Result3 = Equals<boolean | string, string | boolean>; // is true
+ *  type Result1 = Equals<string, string>; // is true
+ *  type Result2 = Equals<number, string>; // is false
+ *  type Result3 = Equals<boolean | string, string | boolean>; // is true
  */
 export type Equals<X, Y> = (<T>() => T extends X ? true : false) extends <
   T,
@@ -192,7 +190,7 @@ export type MaybeUnknown<T> = T | unknown;
 export type MaybeUndefined<T> = T | undefined;
 
 /** 
- A export type that excludes `null` and `undefined` from export type `T`.
+ A  type that excludes `null` and `undefined` from  type `T`.
  * @example
  Type Str = ExcludeNullable<string | null> //  string 
  Type Str2 = ExcludeNullable<string | null | undefined> //  string 
@@ -201,21 +199,21 @@ export type MaybeUndefined<T> = T | undefined;
  */
 export type ExcludeNullable<T> = Exclude<T, Nullable>;
 /** 
- A export type that excludes `undefined` from export type `T`.
+ A  type that excludes `undefined` from  type `T`.
  * @example
  Type Str = ExcludeNullable<string | undefined> // Result:  string 
  */
 export type ExcludeUndefined<T> = Exclude<T, undefined>;
 
 /** 
- A export type that excludes `null` from export type `T`.
+ A  type that excludes `null` from  type `T`.
  * @example
  Type Str = ExcludeNullable<string | null> // Result:  string 
  */
 export type ExcludeNull<T> = Exclude<T, null>;
 
 /**
- * A export type that recursively mutates all the proprties within a given object export type `T`.
+ * A  type that recursively mutates all the proprties within a given object  type `T`.
  */
 export type DeepMutable<T> = T extends UnknownFunction
   ? T
@@ -229,7 +227,7 @@ export type DeepMutable<T> = T extends UnknownFunction
 export type IsDeepMutable<T> = T extends DeepMutable<T> ? true : false;
 
 /**
- * A export type that recursively turns the proprties within a given object export type `T` immutable.
+ * A  type that recursively turns the proprties within a given object  type `T` immutable.
  */
 export type DeepImmutable<T> = T extends UnknownFunction
   ? T
@@ -247,14 +245,14 @@ export type IsDeepImmutable<T> = T extends DeepImmutable<T> ? true : false;
 declare const __s: unique symbol;
 
 /**
- * This export type alias defines a mechanism similar to Python's [`NewType`](https://docs.python.org/3/library/typing.html#newtype).
+ * This  type alias defines a mechanism similar to Python's [`NewType`](https://docs.python.org/3/library/typing.html#newtype).
  * In TypeScript world it's refered to as 'Branding'.
- * The `NewType<N>` encapsulates `N`, thus creating a distinct export type that
- * TypeScript's export type system recognizes as separate from its underlying export type.
+ * The `NewType<N>` encapsulates `N`, thus creating a distinct  type that
+ * TypeScript's  type system recognizes as separate from its underlying  type.
  * @example
- * export type Foo = NewType<string>;
- * export type Bar = NewType<number>;
- * export type Baz = NewType<boolean>;
+ *  type Foo = NewType<string>;
+ *  type Bar = NewType<number>;
+ *  type Baz = NewType<boolean>;
  *
  * function fooBarBaz(foo: Foo, bar: Bar): Baz {
  *   foo.concat(); // Allowed since Foo is an underlying string
@@ -303,21 +301,21 @@ export type DeepOmit<T, P extends string> = P extends `${infer K}.${infer R}`
 
 export type EmptyArray = [];
 /**
- * Represents a export type that transposes a given 2D array `M`.
+ * Represents a  type that transposes a given 2D array `M`.
  * It flips the matrix over its diagonal, switching its row and column indices.
- * @template M - 2D array of any primitive export type values.
+ * @template M - 2D array of any primitive  type values.
  *
  * @example
  * // Transpose a 1x1 matrix
- * export type Matrix = ArrayTranspose<[[1]]>; // expected to be [[1]]
+ *  type Matrix = ArrayTranspose<[[1]]>; // expected to be [[1]]
  *
  * @example
  * // Transpose a 2x2 matrix
- * export type Matrix1 = ArrayTranspose<[[1, 'i'], [3, 4]]>; // expected to be [[1, 3], ["i", 4]]
+ *  type Matrix1 = ArrayTranspose<[[1, 'i'], [3, 4]]>; // expected to be [[1, 3], ["i", 4]]
  *
  * @example
  * // Transpose a 2x3 matrix
- * export type Matrix2 = ArrayTranspose<[[1, true, 3], [4, 5, 6]]>; // expected to be [[1, 4], [true, 5], [3, 6]]
+ *  type Matrix2 = ArrayTranspose<[[1, true, 3], [4, 5, 6]]>; // expected to be [[1, 4], [true, 5], [3, 6]]
  *
  */
 export type ArrayTranspose<
@@ -332,14 +330,14 @@ export type ArrayTranspose<
     };
 
 /**
- * Represents a export type that filters elements from an array based on a given predicate export type.
+ * Represents a  type that filters elements from an array based on a given predicate  type.
  * @typeParam T The array to filter.
  * @typeParam P The predicate used for filtering elements from `T`.
  * @returns a new array type containing only the elements of `T` that match `P`.
  * @example
  * ```typescript
- * export type Numbers = [0, 1, 2, 3];
- * export type FilteredNumbers = Filter<Numbers, 0 | 1>; // Results in [0, 1]
+ *  type Numbers = [0, 1, 2, 3];
+ *  type FilteredNumbers = Filter<Numbers, 0 | 1>; // Results in [0, 1]
  * ```
  */
 export type ArrayFilter<T extends unknown[], P> = T extends [
@@ -352,10 +350,10 @@ export type ArrayFilter<T extends unknown[], P> = T extends [
   : [];
 
 /**
- * Retrieves the keys that are mutable from an object of export type T.
+ * Retrieves the keys that are mutable from an object of  type T.
  * @example
  * ```typescript
- * export type ExampleType = {
+ *  type ExampleType = {
  *   a: number;
  *   readonly b: string;
  *   c: {
@@ -364,7 +362,7 @@ export type ArrayFilter<T extends unknown[], P> = T extends [
  *   };
  * };
  *
- * export type MutableKeysOfExampleType = MutableKeys<ExampleType>;
+ *  type MutableKeysOfExampleType = MutableKeys<ExampleType>;
  * // Result: 'a' | 'c'
  * ```
  */
@@ -375,10 +373,10 @@ export type MutableKeys<T> = {
 }[Keys<T>];
 
 /**
- * Retrieves the keys that are immutable (readonly) from an object of export type T.
+ * Retrieves the keys that are immutable (readonly) from an object of  type T.
  * @example
  * ```typescript
- * export type ExampleType = {
+ *  type ExampleType = {
  *   a: number;
  *   readonly b: string;
  *   c: {
@@ -387,7 +385,7 @@ export type MutableKeys<T> = {
  *   };
  * };
  *
- * export type ImmutableKeysOfExampleType = ImmutableKeys<ExampleType>;
+ *  type ImmutableKeysOfExampleType = ImmutableKeys<ExampleType>;
  * // Result: 'b'
  * ```
  */
@@ -403,8 +401,8 @@ declare function _testType<T1, T2, E extends boolean>(): Equals<
 >;
 /**
  * Determines if two types match.
- * @template T1 The first export type to compare.
- * @template T2 The second export type to compare.
+ * @template T1 The first  type to compare.
+ * @template T2 The second  type to compare.
  * @template Expected A boolean literal indicating whether `T1` should match `T2`.
  * If you expect the types to match, set this to true; if not, set it to false.
  * This utility will return a boolean that is true if your expectation was correct, otherwise false.
@@ -414,15 +412,15 @@ export type TestType<T1, T2, Expected extends boolean> = ReturnType<
 >;
 
 /**
- * Represents a export type that checks if a specified element export type exists within an array.
- * @typeParam T The array export type to search within.
- * @typeParam I The element export type to check for existence in `T`.
+ * Represents a  type that checks if a specified element  type exists within an array.
+ * @typeParam T The array  type to search within.
+ * @typeParam I The element  type to check for existence in `T`.
  * @returns `true` if `I` is found within `T`, otherwise `false`.
  * @example
  * ```typescript
- * export type Numbers = [0, 1, 2];
- * export type CheckZero = IsInArray<Numbers, 0>; // true
- * export type CheckThree = IsInArray<Numbers, 3>; // false
+ *  type Numbers = [0, 1, 2];
+ *  type CheckZero = IsInArray<Numbers, 0>; // true
+ *  type CheckThree = IsInArray<Numbers, 3>; // false
  * ```
  */
 export type IsInArray<T extends unknown[], I> = T extends [infer S, ...infer E]
@@ -432,11 +430,11 @@ export type IsInArray<T extends unknown[], I> = T extends [infer S, ...infer E]
   : false;
 
 /**
- * A export type that constructs a new array containing only unique elements from a given array export type.
- * @typeParam T The input array export type from which unique elements are extracted.
+ * A  type that constructs a new array containing only unique elements from a given array export type.
+ * @typeParam T The input array  type from which unique elements are extracted.
  * @example
  * ```typescript
- * export type UArr = UniqueArray<
+ *  type UArr = UniqueArray<
   [unknown, unknown, 'foo', any, never, never, '33', 33, '33']
 >; // =>  [unknown, 'foo', any, never, '33', 33]
  * ```
