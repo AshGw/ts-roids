c8190c483e8af690cd3986ef1171e0e519bbe1d6
AshGw <AshrefGw@proton.me>
2024-04-23 16:25:02 +0100

    feat(#80): finish `IsInteger<Arr>`

--- src/index.ts
+++ src/index.ts
@@ -135,6 +135,32 @@ export type IsNegative<N extends Numeric> =
   StringifyNum<N> extends `-${infer U}` ? true : false;
 
 /**
+ * Check if a given numeric value is an integer
+ * @returns
+ * true if it is, else false
+ */
+export type IsInteger<N extends Numeric> = number extends N
+  ? false | true
+  : N extends N
+    ? `${N}` extends `${string}.${string}`
+      ? false
+      : true
+    : never;
+
+/**
+ * Check if a given numeric value is an float
+ * @returns
+ * true if it is, else false
+ */
+export type IsFloat<N extends Numeric> = number extends N
+  ? false | true
+  : N extends N
+    ? `${N}` extends `${string}.${string}`
+      ? true
+      : false
+    : never;
+
+/**
  * Checks if a given numeric value is in [0,+∞[
  * @returns
  * true if it is, otherwise false
---
+++ tests/is-integer.test.ts
@@ -0,0 +1,27 @@
+import { IsInteger, TestType } from 'src';
+import { test, expect } from 'vitest';
+
+test('_', () => {
+  const result: TestType<IsInteger<0>, true, true> = true;
+  expect(result).toBe(true);
+});
+
+test('not a limitation', () => {
+  const result: TestType<IsInteger<1000000>, true, true> = true;
+  expect(result).toBe(true);
+});
+
+test('_', () => {
+  const result: TestType<IsInteger<-54>, true, true> = true;
+  expect(result).toBe(true);
+});
+
+test('_', () => {
+  const result: TestType<IsInteger<1>, true, true> = true;
+  expect(result).toBe(true);
+});
+
+test('_', () => {
+  const result: TestType<IsInteger<0.001>, false, true> = true;
+  expect(result).toBe(true);
+});
