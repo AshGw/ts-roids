fc75c13741381b561ba2be0234cb0b5fa501c048
AshGw <AshrefGw@proton.me>
2024-04-24 02:48:00 +0100

    test: add more test cases for `ArrayIncludes` type

--- tests/array-includes.test.ts
+++ tests/array-includes.test.ts
@@ -64,3 +64,31 @@ test('_', () => {
   > = true;
   expect(result).toBe(true);
 });
+
+test('_', () => {
+  const result: TestType<ArrayIncludes<[0, 1, 2], 0>, true, true> = true;
+  expect(result).toBe(true);
+});
+
+test('This is limited to only work on one dimensional arrays', () => {
+  const result: TestType<
+    ArrayIncludes<[0, 1, [69, 22, [99, 88]]], 88>,
+    false,
+    true
+  > = true;
+  expect(result).toBe(true);
+});
+
+test('_', () => {
+  const result: TestType<ArrayIncludes<[99, 88], 88>, true, true> = true;
+  expect(result).toBe(true);
+});
+
+test('_', () => {
+  const result: TestType<
+    ArrayIncludes<[unknown, any], number>,
+    false,
+    true
+  > = true;
+  expect(result).toBe(true);
+});
